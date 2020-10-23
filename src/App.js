4640719bec97324df069594259cad46a12c18578
Dan Johnson <danimal.johnson@gmail.com>
2020-10-23 05:57:40 -0700

    Removes demo code. Adds scss. Rearranges files.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>This is in the App-header section.</h1>
+        <h2>Form goes here.</h2>
       </header>
+      <section className="info-box-section">
+        <h1>This is in the info-box-section.</h1>
+      </section>
+      <section className = "map-section">
+        <h1>This is in the map section</h1>
+      </section>
     </div>
   );
 }
